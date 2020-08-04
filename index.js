0f4675aafdc5d0533038ce3a90b5fe604dca46a7
jhonfmg7 <62905219+jhonfmg7@users.noreply.github.com>
2020-08-04 11:42:16 -0500

    Update index.js

--- index.js
+++ index.js
@@ -30,6 +30,6 @@ app.use('/api/tasks', require('./routes/tasks'));
 // });
 
 // Run app in port
-app.listen(PORT, '0.0.0.0', () => {
+app.listen(port, '0.0.0.0', () => {
     console.log(`Server is run at ${PORT} port`)
 })
