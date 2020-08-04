437fd04ad797ac6b40f25d231b38673748d043d2
jhonfmg7 <62905219+jhonfmg7@users.noreply.github.com>
2020-08-04 11:32:36 -0500

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json({ extended: true }));
 
 // App Port
-const PORT = process.env.PORT || 4000;
+const port = process.env.port || 4000;
 
 // Import Routes
 app.use('/api/users', require('./routes/users'));
@@ -30,6 +30,6 @@ app.use('/api/tasks', require('./routes/tasks'));
 // });
 
 // Run app in port
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is run at ${PORT} port`)
 })
